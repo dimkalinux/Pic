5724da357e08f9e2bbdffdb8235a8ac975a7bdc4
dimkalinux <dimka.linux@gmail.com>
2010-10-29 18:17:31 +0300

    Fix status_stub, remove some jS

--- status_stub.php
+++ status_stub.php
@@ -8,9 +8,8 @@ require AMI_ROOT.'functions.inc.php';
 try {
 	$db = DB::singleton();
 	$row = $db->getRow("SELECT COUNT(*) AS num_files FROM pic");
-	exit($row['num_files']);
+	exit(intval($row['num_files'], 10));
 } catch (Exception $e) {
-	exit('error');
+	exit(0);
 }
-exit($out);
 
